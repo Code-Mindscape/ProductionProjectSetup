d7e2bcdf819a65b6b885f72b5d2e2a0dace4ad24
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-05-23 11:47:57 +0500

    set up mongodb method#1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
 
+import express from "express";
+const app = express();
+
+(async ()=>{
+    try {
+        mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error)=> {
+            console.error("Error: ", error)
+            throw error
+        })
+        app.listen(process.env.PORT)
+    } catch (error) {
+        console.error("ERROR: ",error);
+        throw Error;
+    }
+})()
