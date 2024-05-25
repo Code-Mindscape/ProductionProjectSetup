80a1963e71e17ee46d07fa9a5255b20328b066e7
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-05-26 01:00:31 +0500

    combine user route with async handler

--- src/app.js
+++ src/app.js
@@ -16,5 +16,9 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app };
